17091c7223323546541912c07cf02a68d3faf0c5
Pedro Correa <peter@Pedros-MBP.home>
2023-07-25 22:32:34 -0400

    assignment 3

--- examples/systemcalls/systemcalls.h
+++ examples/systemcalls/systemcalls.h
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdarg.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
 bool do_system(const char *command);
 
--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -10,8 +10,8 @@ WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
 username=$(cat conf/username.txt)
 
-make clean
-make
+# make clean
+# make
 
 if [ $# -lt 3 ]
 then
